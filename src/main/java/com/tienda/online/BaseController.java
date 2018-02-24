9d3f3dcb0b430c416594b97a468baa7176e3c54b
rfre1492 <rfre1492@yahoo.es>
2018-02-24 11:17:24 -0500

    v1

---
+++ src/main/java/com/tienda/online/BaseController.java
@@ -0,0 +1,30 @@
+package com.tienda.online;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tienda.online.modelo.dto.response.ErrorResponse;
+
+@RestController
+public class BaseController {
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NoSuchElementException.class)
+	public ErrorResponse return404(NoSuchElementException ex) {
+		return new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+		
+		
+	}
+	
+	@ResponseStatus(HttpStatus.CONFLICT)
+	@ExceptionHandler(NoSuchElementException.class)
+	public ErrorResponse return409(DataIntegrityViolationException ex) {
+		return new ErrorResponse(ex.getMessage(), HttpStatus.CONFLICT.value());
+		
+		
+	}
+}
---
+++ src/main/java/com/tienda/online/servicio/UsuarioServicio.java
@@ -0,0 +1,49 @@
+package com.tienda.online.servicio;
+
+
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tienda.online.modelo.Usuario;
+import com.tienda.online.repositorio.UsuarioRepositorio;
+
+@Service
+public class UsuarioServicio {
+
+	private UsuarioRepositorio usuarioRepositorio;
+
+	@Autowired
+	public UsuarioServicio(UsuarioRepositorio usuarioRepositorio) {
+		super();
+		this.usuarioRepositorio = usuarioRepositorio;
+	}
+	
+	public Usuario guardar(Usuario usuario) {
+		usuario.setFecha(new Date());
+		Usuario usuarioExiste = usuarioRepositorio.findByEmail(usuario.getEmail());
+		if(usuarioExiste == null) {
+			return usuarioRepositorio.save(usuario);
+		}
+		
+		
+		return null;
+	}
+	
+	
+	public List<Usuario> obtenerTodos(){
+		return (List<Usuario>)usuarioRepositorio.findAll();
+	}
+	
+	public long contar () {
+		return usuarioRepositorio.count();
+	}
+	
+	public void eliminar(Integer id) {
+		usuarioRepositorio.delete(id);
+	}
+	
+}
