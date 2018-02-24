cbd5d0920ec559b53ab17248ca5260b127bf0c15
rfre1492 <rfre1492@yahoo.es>
2018-02-24 11:42:17 -0500

    Esta es una prueba

--- src/main/java/com/tienda/online/BaseController.java
+++ src/main/java/com/tienda/online/BaseController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tienda.online.modelo.dto.response.ErrorResponse;
-
+//Prueba
 @RestController
 public class BaseController {
 	@ResponseStatus(HttpStatus.NOT_FOUND)
